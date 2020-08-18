0ab43a7a0584698bc5d4ea055b3df60de57508b6
codegangsk <kjhj0604@gmail.com>
2020-08-18 23:58:33 +0900

    Initial Commit

---
+++ 3.9LifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3.9LifeCycle
+//
+//  Created by Sophie Kim on 2020/08/18.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
